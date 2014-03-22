45b9c25b51ce0654cf2dad1e9921a242a96f9322
miciek <miciek@gmail.com>
2014-03-22 21:59:18 +0100

    merge sort

--- src/com/michalplachta/hunting/uva/a10810/Main.java
+++ src/com/michalplachta/hunting/uva/a10810/Main.java
@@ -3,9 +3,11 @@ package com.michalplachta.hunting.uva.a10810;
 import java.util.Scanner;
 
 /**
- * UVA 10810 - Contest Scoreboard problem solution
+ * UVA 10810 - Ultra Quicksort problem solution
  * http://uva.onlinejudge.org/index.php?option
  * =com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=1751
+ * 
+ * @category ad-hoc, sorting, merge-sort
  * @author micio
  */
 class Main {
@@ -13,31 +15,31 @@ class Main {
     private final int[] temp = new int[500000];
     private int length = 0;
     private long swaps = 0;
-    
+
     public void add(int c) {
         array[length++] = c;
     }
-    
+
     public void reset() {
         length = 0;
     }
-    
+
     public boolean isSorted() {
-        for(int i = 0; i < length - 1; i++) {
-            if(array[i] > array[i+1]) {
+        for (int i = 0; i < length - 1; i++) {
+            if (array[i] > array[i + 1]) {
                 return false;
             }
         }
-        
+
         return true;
     }
-    
+
     public long getMinSwaps() {
         swaps = 0;
         mergeSort(0, length);
         return swaps;
     }
-    
+
     private void mergeSort(int left, int right) {
         if (left <= right) {
             int size = right - left;
@@ -53,11 +55,11 @@ class Main {
             }
         }
     }
-    
+
     private void merge(int li, final int liend, int ri, final int riend) {
         int i = 0;
         int start = li;
-        while (li < liend) {            
+        while (li < liend) {
             if (ri == riend || array[li] <= array[ri]) {
                 temp[start + i] = array[li];
                 li++;
@@ -66,14 +68,14 @@ class Main {
                 swaps += ri - start - i;
                 ri++;
             }
-            i++;            
+            i++;
         }
-        
+
         for (int j = start; j < start + i; j++) {
             array[j] = temp[j];
         }
     }
-    
+
     public static void main(String[] args) {
         Main m = new Main();
         Scanner in = new Scanner(System.in);
