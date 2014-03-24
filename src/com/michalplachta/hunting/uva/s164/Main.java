c400df95ceb40abed6819ce02b9e9b8fb5854461
micio <miciek@gmail.com>
2014-03-24 17:39:53 +0100

    UVA 164 template

---
+++ src/com/michalplachta/hunting/uva/s164/Main.java
@@ -0,0 +1,18 @@
+package com.michalplachta.hunting.uva.s164;
+
+import java.util.Scanner;
+
+/**
+ * UVA 164 - String Computer
+ * 
+ * http://uva.onlinejudge.org/external/1/164.html
+ * 
+ * @category string, dynamic-programming, edit-distance, alignment
+ * @author micio
+ */
+class Main {   
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/com/michalplachta/hunting/uva/s164/MainTest.java
@@ -0,0 +1,13 @@
+package com.michalplachta.hunting.uva.s164;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    public void example() {       
+        
+    }
+
+}
