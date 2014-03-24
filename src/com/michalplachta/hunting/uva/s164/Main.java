c2b998b572734e86a3be7c983d3be580e77e5fa6
micio <miciek@gmail.com>
2014-03-24 17:57:45 +0100

    added StringAlignment template

--- src/com/michalplachta/hunting/uva/s164/Main.java
+++ src/com/michalplachta/hunting/uva/s164/Main.java
@@ -10,7 +10,32 @@ import java.util.Scanner;
  * @category string, dynamic-programming, edit-distance, alignment
  * @author micio
  */
-class Main {   
+class Main {
+    
+    public static class StringAlignment {
+        private char[] A;
+        private char[] B;
+        private int[][] dp;
+        private final int match;
+        private final int mismatch;
+        private final int deletion; // represented as '\u0000' in B
+        private final int insertion; // represented as '\u0000' in A
+        
+        public StringAlignment(int matchScore, int mismatchScore, int deletionScore, int insertionScore) {
+            match = matchScore;
+            mismatch = mismatchScore;
+            deletion = deletionScore;
+            insertion = insertionScore;
+        }
+        
+        public void solve(String a, String b) {
+            // TODO
+        }
+        
+        public int getScore() {
+            return dp[A.length - 1][B.length - 1];
+        }
+    }
 
     public static void main(String[] args) {
         
