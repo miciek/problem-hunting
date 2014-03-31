349bb1e25b6cf110c0183bec8209b23a81daf0a2
miciek <miciek@gmail.com>
2014-03-31 07:30:04 +0200

    template for UVA 422

--- src/com/michalplachta/hunting/uva/s422/Main.java
+++ src/com/michalplachta/hunting/uva/s422/Main.java
@@ -1,5 +1,8 @@
 package com.michalplachta.hunting.uva.s422;
 
+import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -11,11 +14,13 @@ import java.util.Scanner;
  * @author micio
  */
 class Main {
+    private final int N;
     private final String[] matrix;
     private int rows = 0;
     private final Result result = new Result();
 
     public Main(int N) {
+        this.N = N;
         matrix = new String[N];
     }
 
@@ -24,7 +29,35 @@ class Main {
     }
 
     public Result find(String s) {
+        result.found = false;
+        int currentIndex = 0;
+        for (int row = 0; row < N; row++) {
+            for (int col = 0; col < N; col++) {
+                if (matrix[row].charAt(col) == s.charAt(currentIndex)) {
+                    result.startRow = row;
+                    result.startCol = col;
+                    List<SimpleEntry<Integer, Integer>> candidates = findCharAround(row, col, s.charAt(currentIndex));
+                    while (!candidates.isEmpty()) {
+
+                    }
+                }
+            }
+        }
         return result;
+    }
+
+    private List<SimpleEntry<Integer, Integer>> findCharAround(int row, int col, char c) {
+        List<SimpleEntry<Integer, Integer>> found = new ArrayList<SimpleEntry<Integer, Integer>>(8);
+        for (int i = row - 1; i <= row + 1; i++) {
+            for (int j = col - 1; j <= col + 1; j++) {
+                if (i >= 0 && i < N && j >= 0 && j < N) {
+                    if ((i != 0 || j != 0) && matrix[i].charAt(j) == c) {
+                        found.add(new SimpleEntry<Integer, Integer>(i, j));
+                    }
+                }
+            }
+        }
+        return found;
     }
 
     public static class Result {
--- src/com/michalplachta/hunting/uva/s422/MainTest.java
+++ src/com/michalplachta/hunting/uva/s422/MainTest.java
@@ -18,4 +18,27 @@ public class MainTest {
         assertEquals("Not found", m.find("DISP").toString());
     }
 
+    @Test
+    public void simple2d() {
+        Main m = new Main(2);
+        m.addRow("AB");
+        m.addRow("CD");
+        assertEquals("1,1 1,2", m.find("AB").toString());
+        assertEquals("1,2 1,1", m.find("BA").toString());
+        assertEquals("2,1 2,2", m.find("CD").toString());
+        assertEquals("2,2 2,1", m.find("DC").toString());
+        assertEquals("1,1 2,2", m.find("AD").toString());
+        assertEquals("2,2 1,1", m.find("DA").toString());
+        assertEquals("1,1 2,1", m.find("AC").toString());
+        assertEquals("2,1 1,1", m.find("CA").toString());
+        assertEquals("1,2 2,2", m.find("BD").toString());
+        assertEquals("2,2 1,2", m.find("DB").toString());
+        assertEquals("2,1 1,2", m.find("CB").toString());
+        assertEquals("1,2 2,1", m.find("BC").toString());
+        assertEquals("Not found", m.find("AA").toString());
+        assertEquals("Not found", m.find("BB").toString());
+        assertEquals("Not found", m.find("CC").toString());
+        assertEquals("Not found", m.find("DD").toString());
+    }
+
 }
