ad2a44e257d6ce47a12cfa26294c63d7db6000fe
miciek <miciek@gmail.com>
2015-08-16 12:23:42 +0200

    few mods on the input parsing + run.sh

--- cpp/pa2014/par.cpp
+++ cpp/pa2014/par.cpp
@@ -6,6 +6,8 @@
 
 using namespace std;
 
+// DATA STRUCTURES AND CONSTS
+
 struct Car {
   int index;
   int x;
@@ -16,14 +18,9 @@ const int MAX_CARS = 50005;
 Car init[MAX_CARS];
 Car target[MAX_CARS];
 
-void read_cars_to(Car cars[], int qty) {
-  int x1, y1, x2, y2;
-  for(int i = 0; i < qty; i++) {
-    scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
-    Car c = {i, min(x1, x2), abs((x2 - x1) * (y2 - y1))};
-    cars[i] = c;
-  }
+// DEBUG & I/O HELPERS
 
+void print_debug(Car cars[], int qty) {
   #ifdef DEBUG
   printf("Read %d cars:\n", qty);
   for(int i = 0; i < qty; i++) {
@@ -31,6 +28,14 @@ void read_cars_to(Car cars[], int qty) {
   }
   printf("\n");
   #endif
+}
+
+void read_cars_to(Car cars[], int qty) {
+  int x1, y1, x2, y2;
+  for(int i = 0; i < qty; i++) {
+    scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+    cars[i] = (Car){i, min(x1, x2), fabs((x2 - x1) * (y2 - y1))};
+  }
 }
 
 int main() {
@@ -41,11 +46,14 @@ int main() {
     scanf("%d %d", &n, &w);
 
     #ifdef DEBUG
-    printf("Case %d for n = %d and w = %d\n", T, n, w);
+    printf("==========\nCase %d for n = %d and w = %d\n", T, n, w);
     #endif
 
     read_cars_to(init, n);
     read_cars_to(target, n);
+
+    print_debug(init, n);
+    print_debug(target, n);
   }
   return 0;
 }
---
+++ cpp/pa2014/run.sh
@@ -0,0 +1,2 @@
+g++ par.cpp && ./a.out < par_tests/00.in
+
