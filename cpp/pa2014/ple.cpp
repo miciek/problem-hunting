b0dac7c2485a6bec1ffd781a1a75424da576684c
miciek <miciek@gmail.com>
2015-08-16 19:15:10 +0200

    added PLE task and tests for it

---
+++ cpp/pa2014/ple.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+
+#define DEBUG
+
+#ifdef DEBUG
+#define D(x) x
+#else
+#define D(x)
+#endif
+
+using namespace std;
+
+// DATA STRUCTURES AND CONSTS
+
+void unit_tests();
+
+int main() {
+  D(unit_tests();)
+  return 0;
+}
+
+// UNIT TESTS FOR EACH PART OF THE ALGORITHM
+void unit_tests() {
+}
+
---
+++ cpp/pa2014/run_ple.sh
@@ -0,0 +1,2 @@
+g++ ple.cpp && ./a.out < ple_tests/ple0.in
+
