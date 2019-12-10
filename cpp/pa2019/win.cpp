50a076da4528cb5e328cca7cb948afb8f973015f
miciek <miciek@gmail.com>
2019-12-10 22:58:56 +0100

    Add first solution for WIN (pa2019)

---
+++ cpp/pa2019/win.cpp
@@ -0,0 +1,51 @@
+#include <cstdio>
+#include <cmath>
+#include <cassert>
+#include <string>
+//#define DEBUG
+
+#ifdef DEBUG
+#define D(x); x
+#else
+#define D(x);
+#endif
+
+using namespace std;
+
+int WINES[2002][2002];
+
+int winesAbove(int i, int j) {
+  if(j == 0 || j == i) {
+    WINES[i][j] = i;
+  } else {
+    WINES[i][j] = min(WINES[i - 1][j - 1] + i - j + 1, WINES[i - 1][j] + j + 1);
+  }
+  return WINES[i][j];
+}
+
+int main() {
+  int n = 0;
+  int k = 0;
+  int minyear = 0;
+
+  while(scanf("%d %d", &n, &k) > 0) {
+  scanf("%d", &minyear);
+
+  D(printf("---\nSOLVING n=%d k=%d\n", n, k););
+
+  for(int i = 1; i < n; i++) {
+    int next;
+    for(int j = 0; j < i + 1; j++) {
+      scanf("%d", &next);
+      if(next == 336) {
+        D(printf("\n336 @%d, %d, above: %d, current %d", i, j, winesAbove(i, j), minyear););
+      }
+      if(winesAbove(i, j) < k && next < minyear)
+        minyear = next;
+    }
+  }
+
+  printf("%d\n", minyear);
+  }
+  return 0;
+}
